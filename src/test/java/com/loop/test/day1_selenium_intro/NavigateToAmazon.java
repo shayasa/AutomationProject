e5822785093bd4cd960f79717eda078b1b61174a
aydanshabanli <ashabanli@gmail.com>
2023-11-19 09:45:58 -0500

    commit

---
+++ src/test/java/com/loop/test/day1_selenium_intro/NavigateToAmazon.java
@@ -0,0 +1,28 @@
+package com.loop.test.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigateToAmazon {
+    public static void main(String[] args) {
+            /*
+    navigate to amazon
+    maximize
+    navigate back
+    navigate forward
+    refresh
+     */
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com");
+        driver.manage().window().maximize();
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/loop/test/day1_selenium_intro/SeleniumTest.java
@@ -0,0 +1,35 @@
+package com.loop.test.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) throws InterruptedException {
+        // setting up  th web driver
+        WebDriverManager.chromedriver().setup();
+        // create an instance of webdriver
+        WebDriver driver = new ChromeDriver();
+
+       //Thread.sleep(5000);
+        //  navigate to web page
+        driver.get("https://www.google.com");
+       // driver.navigate().to("https://www.google.com");
+        // maximize
+        driver.manage().window().maximize();
+
+        driver.manage().window().fullscreen();
+        driver.navigate().to("https://www.etsy.com");
+        //navigate back
+        driver.navigate().back();
+        //navigate forward
+        driver.navigate().forward();
+        // navigate refresh
+        driver.navigate().refresh();
+
+        // quit close will close current one
+        driver.quit();
+        // will close all brothers
+        driver.close();
+    }
+}
